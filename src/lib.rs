272eff9fc6b0de617c2125bca556b3315f4296c3
Joseph Quinn <quinn.josephr@gmail.com>
2018-04-16 16:51:33 -0500

    Adding negative test, adding doumentation to lib.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,18 @@
 // Copyright (c) 2018 Joseph R. Quinn <quinn.josephr@protonmail.com>
 // SPDX-License-Identifier: ISC
 
+//! [![Build Status](https://travis-ci.org/quinnjr/is-dev-rs.svg?branch=master)](https://travis-ci.org/quinnjr/is-dev-rs)
+//!
+//! A simple macro to determine if an environment is
+//! a "development" environment.
+//!
+//! Built because some of us (me) are lazy and don't feel like
+//! copy-pasting the same check for development
+//! environments over and over again.
+//!
+//! :heart:
+
+#![allow(unused_imports)]
 use std::env;
 use std::ffi::OsStr;
 
@@ -20,8 +32,18 @@ mod tests {
     use super::*;
 
     #[test]
+    // Only tests positive with ENV=development
+    // set on the testing machine.
     fn test_is_dev() {
         let e: bool = is_dev!("ENV", "development");
-        assert!(e);
+        assert!(e)
+    }
+
+    #[test]
+    fn test_is_not_dev() {
+        let e: bool = is_dev!("NODE_ENV", "development");
+        assert!(!e);
+        let f: bool = is_dev!("SUPERSEKRITKEY", "development");
+        assert!(!f)
     }
 }
